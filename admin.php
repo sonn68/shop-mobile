403fcc193fc7f960e8632e5f6e0a858952e269bb
ngo the son <ngotheson1@gmail.com>
2019-10-14 14:36:46 +0700

    fix login

--- admin.php
+++ admin.php
@@ -2,15 +2,15 @@
 	session_start();
 	include "config.php";
 	include "model/model.php";
-	$session = $_SESSION["username"];
-	if(isset($session)==false)
+	$session = isset($_SESSION["username"]) ? $_SESSION["username"] : null;
+	if(!$session)
 		include("controller/frontend/controller_login.php");
 	else{
 		checkAdmin($session);
 	}
 	function checkAdmin ($session) {
 		$model=new model();
-		$role = $model->get_a_record("select role from user where username='$session' ");
+		$role = $model->get_a_record("select role from User where username='$session' ");
 		if($role->role == "admin"){
 			$controller=isset($_GET["controller"])?$_GET["controller"]:"";
 			$controller="controller/backend/controller_$controller.php";
