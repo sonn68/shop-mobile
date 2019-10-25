429edc1d76128346aad9e135c23e23235781558e
Ngadang-2701 <56036890+Ngadang-2701@users.noreply.github.com>
2019-10-26 00:13:05 +0700

    pushing in 26/10

--- view/frontend/view_slider.php
+++ view/frontend/view_slider.php
@@ -4,13 +4,18 @@
                 <ul class="" id="bxslider-home4">
                     <?php foreach ($arr_slide as $row){ ?>
                     <li>
-                        <img style="height: 236px;width: 743px;" src="public/image/image_in_DB/<?php echo $row->image?>" alt="Slide">
-                        <div class="caption-group">
-                            <h2 class="caption title" >
+                        <div style="width:30%; height: 30%; margin-left: 7vw;">
+                            <img class="image-slider" src="public/image/image_in_DB/<?php echo $row->image?>" alt="Slide">
+                        </div>                
+                        <div class="caption-group" >
+                            <div style="width: 350px; height: 300px; left: 50%;  word-wrap: break-word;">
+                                <h2 style="font-size: 2.5vw" class="caption title" >
                                 <span class="primary"> <strong><?php echo $row->name?></strong></span>
                             </h2>
                             <!-- <h4 class="caption subtitle">Dual SIM</h4> -->
                             <a class="caption button-radius" href="#"><span class="icon"></span>Shop now</a>
+                            </div>
+                            
                         </div>
                     </li>
                     <?php } ?>
