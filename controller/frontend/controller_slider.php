ce181006abeb572d6c6a04cad28f929d43a4ab30
sontranptit <tnnsonptit@gmail.com>
2019-10-25 13:40:11 +0700

    slider

--- controller/frontend/controller_slider.php
+++ controller/frontend/controller_slider.php
@@ -1,16 +1,12 @@
 
-<?php 
-include ("view/frontend/view_slider.php");
-		
-	/*
+<?php 	
 	class controller_slide {
 	 	public $model;
 		public function __construct(){
 			$this->model= new model();
-			$arr= $this->model->get_all("select * from slide  desc limit 0, 3");
+			$arr_slide=$this->model->get_all("select * from product order by rand() desc limit 1,4 ");
 			include ("view/frontend/view_slider.php");
 		}
 	}
-	new controller_slide();
-	*/ 		
+	new controller_slide();	
  ?>
--- view/frontend/view_slider.php
+++ view/frontend/view_slider.php
@@ -2,43 +2,20 @@
             <!-- Slider -->
             <div class="block-slider block-slider4">
                 <ul class="" id="bxslider-home4">
+                    <?php foreach ($arr_slide as $row){ ?>
                     <li>
-                        <img src="img/h4-slide.png" alt="Slide">
+                        <img style="height: 236px;width: 525px;" src="public/image/image_in_DB/<?php echo $row->image?>" alt="Slide">
                         <div class="caption-group">
-                            <h2 class="caption title">
-                                iPhone <span class="primary">6 <strong>Plus</strong></span>
+                            <h2 class="caption title" >
+                                <span class="primary"> <strong><?php echo $row->name?></strong></span>
                             </h2>
-                            <h4 class="caption subtitle">Dual SIM</h4>
+                            <!-- <h4 class="caption subtitle">Dual SIM</h4> -->
                             <a class="caption button-radius" href="#"><span class="icon"></span>Shop now</a>
                         </div>
                     </li>
-                    <li><img src="img/h4-slide2.png" alt="Slide">
-                        <div class="caption-group">
-                            <h2 class="caption title">
-                                by one, get one <span class="primary">50% <strong>off</strong></span>
-                            </h2>
-                            <h4 class="caption subtitle">school supplies & backpacks.*</h4>
-                            <a class="caption button-radius" href="#"><span class="icon"></span>Shop now</a>
-                        </div>
-                    </li>
-                    <li><img src="img/h4-slide3.png" alt="Slide">
-                        <div class="caption-group">
-                            <h2 class="caption title">
-                                Apple <span class="primary">Store <strong>Ipod</strong></span>
-                            </h2>
-                            <h4 class="caption subtitle">Select Item</h4>
-                            <a class="caption button-radius" href="#"><span class="icon"></span>Shop now</a>
-                        </div>
-                    </li>
-                    <li><img src="img/h4-slide4.png" alt="Slide">
-                        <div class="caption-group">
-                          <h2 class="caption title">
-                                Apple <span class="primary">Store <strong>Ipod</strong></span>
-                            </h2>
-                            <h4 class="caption subtitle">& Phone</h4>
-                            <a class="caption button-radius" href="#"><span class="icon"></span>Shop now</a>
-                        </div>
-                    </li>
+                    <?php } ?>
+                    
+                    
                 </ul>
             </div>
             <!-- ./Slider -->
