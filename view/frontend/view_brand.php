94a3ad888767d22523ab74e591cd94ab0a8faf27
Ngadang-2701 <56036890+Ngadang-2701@users.noreply.github.com>
2019-11-03 00:43:08 +0700

    Revert "Revert "update image folder""
    This reverts commit 823f811149a2a8ad931d669e6ebb242584e5e74d.

--- view/frontend/view_brand.php
+++ view/frontend/view_brand.php
@@ -6,11 +6,12 @@
                 
                 <div class="brand-wrapper">
                     <div class="brand-list"> 
-                        <?php foreach ($arr as $row) { ?>
-                        <a href="product/category/<?php echo($row->id) ?>">
-                            <img src="public/image/image_in_DB/<?php echo ($row->image) ?>"  >
-                        </a>  
-                        <?php } ?>
+                        <a href="product/category/ "> <img src="public/image/image_in_DB/apple.png"></a>
+                        <a href="product/category/ "> <img src="public/image/image_in_DB/samsung.png"></a>
+                        <a href="product/category/ "> <img src="public/image/image_in_DB/dell.png"></a>
+                        <a href="product/category/ "> <img src="public/image/image_in_DB/acer.jpg"></a>
+                        <a href="product/category/ "> <img src="public/image/image_in_DB/nokia.png"></a>
+                        <a href="product/category/ "> <img src="public/image/image_in_DB/lenovo.png"></a>
                     </div>
                 </div>
                 
@@ -18,3 +19,9 @@
         </div>
     </div>
 </div>
+
+<!-- <?php foreach ($arr as $row) { ?>
+                        <a href="product/category/<?php echo($row->id) ?>">
+                            <img src="public/image/image_in_DB/<?php echo ($row->image) ?>"  >
+                        </a>  
+                        <?php } ?> -->
