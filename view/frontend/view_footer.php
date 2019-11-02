8a9556aaeb24fd3be789c799243e40450ca134cc
Ngadang-2701 <56036890+Ngadang-2701@users.noreply.github.com>
2019-11-02 15:45:10 +0700

    2/11

--- view/frontend/view_footer.php
+++ view/frontend/view_footer.php
@@ -1,3 +1,4 @@
+
 <div class="footer-top-area">
         <div class="zigzag-bottom"></div>
         <div class="container">
@@ -6,12 +7,7 @@
                     <div class="footer-about-us">
                         <h2>u<span>Stora</span></h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
-                        <div class="footer-social">
-                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
-                        </div>
+                        
                     </div>
                 </div>
                 
